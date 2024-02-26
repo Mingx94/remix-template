a26468a200b116bbaa9dbad5145a87fc2a91d360
Michael Tsai <mingxcv@gmail.com>
2024-02-26 22:21:06 +0800

    feat: update packages

--- remix.config.ts
+++ remix.config.ts
@@ -1,4 +1,4 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config";
